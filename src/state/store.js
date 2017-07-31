0e7f0754576924871934b50a7fe54e2f8429a5e1
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-07-31 12:37:10 -0600

    Cleaned out unneeded application cruft.

--- src/state/store.js
+++ src/state/store.js
@@ -1,21 +1,16 @@
 
-import {applyMiddleware, compose, createStore} from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux'
 
-import {sagas} from 'state/sagas'
-import {middleware as routesMiddleware, enhancer as routesEnhancer} from 'state/routes'
-import {reducers} from 'state/reducers'
+import {enhancer, middleware, reducer} from 'state/routes'
 
-const sagasMiddleware = createSagaMiddleware()
-
-const composeMiddlewares = applyMiddleware(routesMiddleware, sagasMiddleware)
+const composeMiddlewares = applyMiddleware(middleware)
 
 // Use Redux DevTools Extension if available and not in production.
 const composeEnhancers = ((process.env.NODE_ENV !== 'production') && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 export const store = createStore(
-  reducers,
-  composeEnhancers(routesEnhancer, composeMiddlewares)
+  combineReducers({
+    location: reducer
+  }),
+  composeEnhancers(enhancer, composeMiddlewares)
 )
-
-sagasMiddleware.run(sagas)
--- src/view/App.js
+++ src/view/App.js
@@ -1,24 +1,13 @@
 
 import React from 'react'
-import styled, {ThemeProvider} from 'styled-components'
+import {ThemeProvider} from 'styled-components'
 
-import {OverlayLoader} from 'view/Shared/OverlayLoader'
-import {Header} from 'view/Shared/Header'
 import {Routes} from 'view/Routes'
 
 import {theme} from 'view/theme'
 
-// GLOBAL WRAPPER
-const Wrapper = styled.div`
-  text-align: center;
-`
-
 export const App = () => (
   <ThemeProvider theme={theme}>
-    <Wrapper>
-      <OverlayLoader />
-      <Header />
-      <Routes />
-    </Wrapper>
+    <Routes />
   </ThemeProvider>
 )
