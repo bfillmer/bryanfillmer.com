c68fab35eb1c112457091f6ec80380aef94dc9f1
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-07-31 18:22:37 -0600

    More cleanup.

--- src/actions.js
+++ src/actions.js
@@ -1,22 +1,8 @@
 
 import {createAction} from 'redux-actions'
 
-import * as types from 'types'
+import {ROUTE_ABOUT, ROUTE_HOME} from 'types'
 
 // ROUTING
-export const routeHome = createAction(types.ROUTE_HOME)
-export const routeAbout = createAction(types.ROUTE_ABOUT)
-
-// MESSAGES
-export const addMessage = createAction(
-  types.ADDED_MESSAGE,
-  (id, type, view, payload) => ({id, type, view, payload})
-)
-export const removeMessage = createAction(types.REMOVED_MESSAGE)
-
-// INTERACTIONS
-export const addInteraction = createAction(types.ADDED_INTERACTION)
-export const removeInteraction = createAction(types.REMOVED_INTERACTION)
-
-// PROFILE
-export const updateName = createAction(types.UPDATED_NAME)
+export const routeAbout = createAction(ROUTE_ABOUT)
+export const routeHome = createAction(ROUTE_HOME)
--- src/state/routes.js
+++ src/state/routes.js
@@ -2,11 +2,11 @@
 import { connectRoutes } from 'redux-first-router'
 import createHistory from 'history/createBrowserHistory'
 
-import {ROUTE_HOME, ROUTE_ABOUT} from 'types'
+import {ROUTE_ABOUT, ROUTE_HOME} from 'types'
 
 const routesMap = {
-  [ROUTE_HOME]: '/',
-  [ROUTE_ABOUT]: '/about'
+  [ROUTE_ABOUT]: '/about',
+  [ROUTE_HOME]: '/'
 }
 
 const history = createHistory()
