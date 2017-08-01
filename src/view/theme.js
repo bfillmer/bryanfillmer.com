653118d95dd0def8f8954c0e07e2bd42e7465abd
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-07-31 18:34:55 -0600

    Created color theme.

--- src/view/theme.js
+++ src/view/theme.js
@@ -1,13 +1,27 @@
 
+import {darken, lighten} from 'polished'
+
+// https://coolors.co/c1cfda-f7f7f2-59f8e8-941c2f-03191e
+const [
+  columbiaBlue,
+  whiteSmoke,
+  electricBlue,
+  vividBurgundy,
+  richBlack
+] = ['#c1cfda', '#f7f7f2', '#59f8e8', '#941c2f', '#03191e']
+
+export const theme = {
+  primary: vividBurgundy,
+  accent: electricBlue,
+  white: whiteSmoke,
+  black: richBlack,
+  grays: {
+    light: lighten(0.5, columbiaBlue),
+    medium: columbiaBlue,
+    dark: darken(0.5, columbiaBlue)
+  }
+}
+
 // Simple helper function, takes in any number of props mapping to properties within the theme
 // object and returns the value.
 export const getTheme = (...props) => ({theme}) => props.reduce((t, p) => t[p], theme)
-
-export const theme = {
-  white: '#f4f4f4',
-  grays: {
-    light: '#eee',
-    medium: '#999',
-    dark: '#333'
-  }
-}
