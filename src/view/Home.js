8d91d7c559e512a241bcf5b084c3e16a3c9f090d
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-07-31 19:25:08 -0600

    Svgs!

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,6 +1,25 @@
 
 import React from 'react'
+import styled from 'styled-components'
+
+import {getTheme} from 'view/theme'
+
+import {Github} from 'view/Svg/Github'
+import {Linkedin} from 'view/Svg/Linkedin'
+
+const Section = styled.section`
+  width: 100%;
+  height: 100vh;
+  background-color: ${getTheme('black')}
+`
 
 export const Home = () => (
-  <p />
+  <Section>
+    <h1>Bryan Fillmer</h1>
+    <hr />
+    <ul>
+      <li><a href='https://github.com/bfillmer/'><Github /> Github</a></li>
+      <li><a href='https://www.linkedin.com/in/bfillmer/'><Linkedin /> Linkedin</a></li>
+    </ul>
+  </Section>
 )
